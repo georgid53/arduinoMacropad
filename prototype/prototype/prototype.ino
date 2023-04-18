b231442a380bdc071f084345c23882fdd427d76e
Georgi <92124079+georgid53@users.noreply.github.com>
2023-04-18 13:03:28 +0300

    Add files via upload

---
+++ prototype/prototype/prototype.ino
@@ -0,0 +1,21 @@
+#include <Keyboard.h> 
+
+const int buttonPin = 4;
+size_t buttonState = 0;
+size_t prevButtonState = HIGH;
+
+int main(void) {
+  pinMode(buttonPin, INPUT_PULLUP);
+  digitalWrite(buttonPin, HIGH);
+  Keyboard.begin();
+
+  while(1){
+    buttonState = digitalRead(buttonPin);
+    if ((buttonState != prevButtonState) && (buttonState == HIGH)) {
+      Keyboard.press('v');
+      delay(100);
+      Keyboard.releaseAll(); // This is important after every Keyboard.press it will continue to be pressed
+  }
+  prevButtonState = buttonState;
+  }
+}
