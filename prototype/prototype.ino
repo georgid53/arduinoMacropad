4a9581cd24b653a61d3a21ee2e55f8d9ba657f7e
Georgi <92124079+georgid53@users.noreply.github.com>
2023-04-21 14:13:55 +0300

    Update prototype.ino

--- prototype/prototype.ino
+++ prototype/prototype.ino
@@ -1,23 +1,27 @@
 #include <Keyboard.h>
- 
+
 const size_t buttonPin = 4;
 extern size_t buttonState = 0;
 extern size_t prevButtonState = HIGH;
-const char keyboardButton = 'k';
- 
+const char keyboardButton[] = "k";
+const int sizeOfArray = sizeof(keyboardButton)/sizeof(keyboardButton[0]);
+
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   digitalWrite(buttonPin, HIGH);
   Keyboard.begin();
 }
- 
+
 void loop() {
   buttonState = digitalRead(buttonPin);
  
   if ((buttonState !=  prevButtonState) && (buttonState == HIGH)){
-    Keyboard.press(keyboardButton);
-    delay(100);
-    Keyboard.releaseAll();
+    for (size_t idx = 0; idx < sizeOfArray; idx++){
+      Keyboard.press(keyboardButton[idx]);
+      delay(100);
+      Keyboard.releaseAll();
+    }
+    
   }
   
   prevButtonState = buttonState;
